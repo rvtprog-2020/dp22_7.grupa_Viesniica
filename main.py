883a44903697e29f3f7c77d263b70d15eb5765a5
DanKun-XS <danila.kunavin1234@gmail.com>
2020-11-26 23:47:52 +0200

    thirth commit

--- main.py
+++ main.py
@@ -14,4 +14,16 @@ def mainpage2():
 def sign_in():
     return render_template('sign_in.html')
 
+@app.route('/croom.html')
+def croom():
+    return render_template('croom.html')
+
+@app.route('/help.html')
+def help():
+    return render_template('help.html')
+
+@app.route('/catologue.html')
+def catologue():
+    return render_template('catologue.html')
+
 app.run(host='0.0.0.0', port=80, debug=True)
